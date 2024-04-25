184207e409d2231da518f31bac65c7d7d1a49b07
付维斌 <phuweibin@qq.com>
2024-04-25 17:37:41 +0800

    refactor: 去掉web.xml中的字符过滤器，使用springboot自动配置实现

--- src/main/java/com/Jspmzlh3zApplication.java
+++ src/main/java/com/Jspmzlh3zApplication.java
@@ -3,9 +3,13 @@ package com;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @MapperScan("com.dao")
 @SpringBootApplication
-public class Jspmzlh3zApplication {
+@ServletComponentScan
+public class Jspmzlh3zApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Jspmzlh3zApplication.class, args);
     }
