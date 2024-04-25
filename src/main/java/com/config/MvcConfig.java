9dbace909124263da31680c30514ee54560f883f
付维斌 <phuweibin@qq.com>
2024-04-25 19:23:01 +0800

    refactor: 去掉spring-mvc.xml文件：使用配置类实现自定义InternalResourceViewResolver的配置

---
+++ src/main/java/com/config/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+        registry.viewResolver(viewResolver);
+    }
+}
